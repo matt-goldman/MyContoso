526457cea90872b12d55bf965a2ab668a1074a7f
Matt Goldman <matt.goldman@outlook.com>
2026-01-17 12:24:48 +1100

    Added IcoFont

--- src/UI/MyContoso.App/MauiProgram.cs
+++ src/UI/MyContoso.App/MauiProgram.cs
@@ -16,6 +16,7 @@ namespace MyContoso.App
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("icofont.ttf", "IcoFont");
                 })
                 .UseMauiCommunityToolkit();
 
